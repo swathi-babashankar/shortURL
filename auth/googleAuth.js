49ef570dc2f47840212eac4d3c0810934a1a99c0
swathi k <swathikbabashankar@gmail.com>
2025-02-09 11:56:56 +0530

    Updated googleAuth

--- auth/googleAuth.js
+++ auth/googleAuth.js
@@ -1,1 +1,10 @@
+
 const {OAuth2Client} = require("google-auth-library");
+
+export default function auth() {
+    const googleAuth = new OAuth2Client({
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET
+    })
+}
+// ProjectId shorturl-450406
